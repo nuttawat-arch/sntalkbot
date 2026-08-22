169cfac9f714d0f4bfa838f39ec6702d1c2d5e33
nuttawat-arch <kuchuxnathkhircathamixakhrab@gmail.com>
2026-08-23 04:10:13 +0700

    Update repositories 2026-08-23 04:10:07

--- run_bot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if not exist .venv\Scripts\python.exe (
-  echo Virtual environment not found. Run setup.bat first.
-  exit /b 1
-)
-.venv\Scripts\python.exe main.py %*
-endlocal
